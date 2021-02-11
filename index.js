c748e3766427c162259e3a39b2b5a11a23824886
Polunosik Denis <denispolunosik@gmail.com>
2021-02-11 23:30:43 +0300

    feat: add cowsay

---
+++ index.js
@@ -0,0 +1,24 @@
+let cowsay = require('cowsay');
+let request = require('request');
+
+let params = {
+    url: "http://helloacm.com/api/fortune",
+    method: "GET"
+};
+
+let callback = function(error, response, body){
+    if (!error) {
+        if (response.statusCode === 200) {
+            console.log(cowsay.say({
+                text: body,
+                e: "Oo",
+                T: "U"
+            })
+            );
+        }
+    } else {
+        console.log("Network error", error);
+    }
+};
+
+request(params, callback);
